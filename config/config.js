cd6dd9645b96668218961ecde1d7aeed64c51d1c
acecode009 <acecode009@gmail.com>
2017-02-17 22:38:09 +0200

    twitter app

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'KHj4Ufp5yvbrau2uZzt6K57TV',
+  consumer_secret:      'Zhb3Bfv8rEdyDoGcJ73epRTwvI8o2jNVOwGR18Sceo6NP7oaO1',
+  access_token:         '832248260655996929-RyopvucyRDlRlE9BUoTjkjD7wp5njDi',
+  access_token_secret:  '3Ltm1WSkxqV2GzYSU7FBXsZ5W5PKCkW0HIRTQEWHFDxFt',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
