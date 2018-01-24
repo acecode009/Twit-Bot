371dbfa6d0d284d666dceb468dffa5cb8ebd34e9
acexode <acecode009@gmail.com>
2018-01-24 15:30:51 +0200

    ignore node_modules

--- app.js
+++ app.js
@@ -3,29 +3,28 @@ var config = require("./config/config")
 console.log(config)
 var T = new Twit(config)
 
-var param = 
-    { 
-        q: 'node.js',
-        count: 2 
-    }
-var tweet = {
-    status:  "http://tutorialzine.com/2016/12/the-languages-frameworks-tools-you-should-learn-in-2017/"
+var param = {
+    q: 'node.js',
+    count: 2
 }
-//T.get('search/tweets',param,getTweets)
-T.post('statuses/update',tweet,posted)
+var tweet = {
+        status: "Hello world from twitter bot"
+    }
+    //T.get('search/tweets',param,getTweets)
+T.post('statuses/update', tweet, posted)
 
 function getTweets(err, data, response) {
     var tweets = data.statuses
-    for(var i=0; i<tweets.length;i++){
-         console.log(i +' ' + tweets[i].text)
+    for (var i = 0; i < tweets.length; i++) {
+        console.log(i + ' ' + tweets[i].text)
     }
- 
+
 }
 
-function posted(err,data,response){
-    if(err){
+function posted(err, data, response) {
+    if (err) {
         console.log("Something went wrong")
-    }else{
+    } else {
         console.log("tweeted")
     }
 }
--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'KHj4Ufp5yvbrau2uZzt6K57TV',
-  consumer_secret:      'Zhb3Bfv8rEdyDoGcJ73epRTwvI8o2jNVOwGR18Sceo6NP7oaO1',
-  access_token:         '832248260655996929-RyopvucyRDlRlE9BUoTjkjD7wp5njDi',
-  access_token_secret:  '3Ltm1WSkxqV2GzYSU7FBXsZ5W5PKCkW0HIRTQEWHFDxFt',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    consumer_key: 'Your consumer key',
+    consumer_secret: 'your consumer secret',
+    access_token: 'your access token',
+    access_token_secret: 'your access token secret',
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
 }
